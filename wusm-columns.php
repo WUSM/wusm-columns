683c0725e7d5f13d3221c1693dfa89e214d87ec8
Aaron Graham <aegraham@gmail.com>
2014-02-14 20:44:10 -0600

    github updater config

--- wusm-columns.php
+++ wusm-columns.php
@@ -8,6 +8,36 @@ Version: 0.1
 Author URI: 
 */
 
+add_action( 'init', 'github_plugin_updater_wusm_columns_init' );
+function github_plugin_updater_wusm_columns_init() {
+
+		if( ! class_exists( 'WP_GitHub_Updater' ) )
+			include_once 'updater.php';
+
+		if( ! defined( 'WP_GITHUB_FORCE_UPDATE' ) )
+			define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+		if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+
+				$config = array(
+						'slug' => plugin_basename( __FILE__ ),
+						'proper_folder_name' => 'wusm-columns',
+						'api_url' => 'https://api.github.com/repos/coderaaron/wusm-columns',
+						'raw_url' => 'https://raw.github.com/coderaaron/wusm-columns/master',
+						'github_url' => 'https://github.com/coderaaron/wusm-columns',
+						'zip_url' => 'https://github.com/coderaaron/wusm-columns/archive/master.zip',
+						'sslverify' => true,
+						'requires' => '3.0',
+						'tested' => '3.8',
+						'readme' => 'README.md',
+						'access_token' => '',
+				);
+
+				new WP_GitHub_Updater( $config );
+		}
+
+}
+
 class tinymce_dashicon_plugin {
 	public function __construct() {
 		add_action( 'admin_init', array( $this, 'admin_init' ) );
